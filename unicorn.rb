610ed8edce0a4c95d5dbba9ea7e1118afbf68b93
Lino <me@awesomelionel.com>
2014-07-28 14:25:26 +0800

    Modify unicorn.rb file to ensure the task :restart works now"

--- unicorn.rb
+++ unicorn.rb
@@ -1,5 +1,8 @@
 @dir = '/var/www/mina.razzle.io/'
 
+#Required for rolling restart
+preload_app true
+
 worker_processes 2
 working_directory "#{@dir}/current"
 
@@ -11,3 +14,16 @@ pid "#{@dir}tmp/pids/unicorn.pid"
 
 stderr_path "#{@dir}log/unicorn.stderr.log"
 stdout_path "#{@dir}log/unicorn.stdout.log"
+
+#Require for rolling restart. It cleans up the old master process
+#once the new master process is started
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
